94b39058f80f5bf375c628630d71a9d457130d1d
hritik-m <99165726+hritik-m@users.noreply.github.com>
2022-02-11 17:09:45 +0530

    lol

--- src/inputverify.java
+++ src/inputverify.java
@@ -24,7 +24,7 @@ public class inputverify {
         caps.setCapability("os", "Windows");
         caps.setCapability("name", "Sample Test");
         caps.setCapability("build", "BStack test");
-        caps.setCapability("browserstack.local", "true");
+       // caps.setCapability("browserstack.local", "true");
 
         driver = new RemoteWebDriver(new URL(URL), caps);
 
