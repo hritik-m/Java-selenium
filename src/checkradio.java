799f1faa2d4ecd1724c4bfa5e6723c097502f8b2
hritik-m <99165726+hritik-m@users.noreply.github.com>
2022-02-11 17:06:40 +0530

    lol

--- src/checkradio.java
+++ src/checkradio.java
@@ -21,7 +21,7 @@ public class checkradio {
         caps.setCapability("os", "Windows");
         caps.setCapability("name", "Sample Test");
         caps.setCapability("build", "BStack test");
-        caps.setCapability("browserstack.local", "true");
+        //caps.setCapability("browserstack.local", "true");
 
         driver = new RemoteWebDriver(new URL(URL), caps);
 
