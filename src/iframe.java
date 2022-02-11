727980b663a9836e89c14c9e19fdf297b1f36ffc
hritik-m <99165726+hritik-m@users.noreply.github.com>
2022-02-11 17:08:26 +0530

    lol

--- src/iframe.java
+++ src/iframe.java
@@ -23,7 +23,7 @@ public class iframe {
         caps.setCapability("os", "Windows");
         caps.setCapability("name", "Sample Test");
         caps.setCapability("build", "BStack test");
-        caps.setCapability("browserstack.local", "true");
+       // caps.setCapability("browserstack.local", "true");
 
         driver = new RemoteWebDriver(new URL(URL), caps);
 
